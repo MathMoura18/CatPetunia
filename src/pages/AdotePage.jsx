62ad325ce8ae470f7c594393f9c6af09c3f98f46
JoaoVitorLB <bjoaovitorbonifacio@gmail.com>
2023-11-07 10:37:04 -0300

    joao-contatoPage-07-11

---
+++ src/pages/AdotePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainLayout from "../layout/MainLayout";
+import AdotePageLayout from "../layout/AdotePageLayout/AdoteLayout.jsx";
+
+function AdotePage() {
+  return (
+    <MainLayout>
+      <AdotePageLayout>
+      </AdotePageLayout>
+    </MainLayout>
+  );
+}
+
+export default AdotePage;
---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from '../layout/MainLayout';
+import AppLayout from '../layout/AppLayout/AppLayout.jsx';
+
+function App() {
+  return(
+    <MainLayout>
+      <AppLayout></AppLayout>
+    </MainLayout>
+  );
+}
+
+export default App
