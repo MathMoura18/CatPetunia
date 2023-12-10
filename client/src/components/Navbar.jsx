e6d687e35a8bbb296d44cbb51ef7889f125f4733
Matheus Moura <mathmo.ura242@gmail.com>
2023-12-10 00:43:20 -0300

    Header fixo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,60 +3,70 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" id="logo">
-          <Link to="/"><h1><i class="fa-solid fa-shield-dog"></i>Cat Petúnia</h1></Link> 
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav ms-auto">
-            <ul class="nav nav-underline">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">
-                  <Link to="/">Home</Link>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">
-                  <Link to="/adote">Adote</Link>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <Link to="/aboutus">Sobre nós</Link>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <Link to="/contact">Contato</Link>
-                </a>
-              </li>
-              <li>
-                <button className="button-link">
-                  <Link to="/contribua"><a href="#">Contribua</a></Link>
-                </button>
-              </li>
-              <li>
-                <button className="button-link">
-                  <Link to="/login"><a href="#">Login</a></Link>
-                </button>
-              </li>
-            </ul>
+    <header>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" id="logo">
+            <Link to="/">
+              <h1>
+                <i class="fa-solid fa-shield-dog"></i>Cat Petúnia
+              </h1>
+            </Link>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+              <ul class="nav nav-underline">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">
+                    <Link to="/">Home</Link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">
+                    <Link to="/adote">Adote</Link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <Link to="/aboutus">Sobre nós</Link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <Link to="/contact">Contato</Link>
+                  </a>
+                </li>
+                <li>
+                  <button className="button-link">
+                    <Link to="/contribua">
+                      <a href="#">Contribua</a>
+                    </Link>
+                  </button>
+                </li>
+                <li>
+                  <button className="button-link">
+                    <Link to="/login">
+                      <a href="#">Login</a>
+                    </Link>
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
 
